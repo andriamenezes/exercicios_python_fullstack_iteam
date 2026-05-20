eb3652bfe0af91a0cef66f83747bcc91ec1bdd4a
andriamenezes <andriamenezes16@gmail.com>
2026-05-19 21:13:45 -0400

    descrição da alteração

---
+++ exercicio7.py
@@ -0,0 +1,4 @@
+largura= 12.5
+comprimento = 30.0
+area= largura*comprimento
+print(f"Area do terreno: {area} m2")
---
+++ exercicio8.py
@@ -0,0 +1,10 @@
+#conversor de celsius para fahrenheit
+
+celsius = 36.5
+
+fahrenheit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15
+
+print (f"Celsius: {celsius}C")
+print(f"Fahrenheit {fahrenheit: .2f}F")
+
